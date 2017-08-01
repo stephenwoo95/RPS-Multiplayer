fdbb50fe1cda17e5ce4cc9cf5088e48b480e7290
Stephen Woo <stephenwoo95@gmail.com>
2017-08-01 01:18:25 -0700

    init file setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA4NaVu5YI_rjxBeeJptYXZOUCs_n2wSyE",
+    authDomain: "rps-multiplayer-c2d3b.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-c2d3b.firebaseio.com",
+    projectId: "rps-multiplayer-c2d3b",
+    storageBucket: "",
+    messagingSenderId: "342841719563"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
